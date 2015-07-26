cf24408c0c1c1ecffe8eb096e10daab7a9cf062a
Brayden Neale <brayden.neale@gmail.com>
2015-07-26 20:42:54 +0800

    updated form for type_of to be a drop down list

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,5 +1,6 @@
 class Company < ActiveRecord::Base
 
+	TYPES = ['Startup', 'Accelerator', 'Incubator', 'Investor', 'Coworking', 'Consulting', 'Event', 'Hackspace']
 	geocoded_by :address
 	after_validation :geocode
 end
