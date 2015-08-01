e0609faeaf6a25543b3ab3a75ce8165cfd88bee3
Brayden Neale <brayden.neale@gmail.com>
2015-08-01 12:42:53 +0800

    bug fixed... syntax in sql query - so searching incorrectly...

--- app/controllers/search_suggestions_controller.rb
+++ app/controllers/search_suggestions_controller.rb
@@ -1,6 +1,6 @@
 class SearchSuggestionsController < ApplicationController
 	def index
-		render json: %w[foo bar]
-		# render json: SearchSuggestion.terms_for(params[:term])
+		# render json: %w[foo bar]
+		render json: SearchSuggestion.terms_for(params[:term])
 	end
 end
