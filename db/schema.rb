93a6e093ecac3558ce501fd58d60ec6a3140c64b
Brayden Neale <brayden.neale@gmail.com>
2015-08-08 13:46:31 +0800

    refactor: setting up for heroku deploy
    - switched db over to postgresql
    - added procfile and puma.rb files needed by heroku/foreman
    - added heroku specific gems to gemfile
    - updated database.yml for postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150808052926) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "companies", force: :cascade do |t|
     t.float    "latitude"
     t.float    "longitude"
